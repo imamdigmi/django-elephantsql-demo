a7f22eef149c228837f14cbb3c9a00baf4ba87d6
imamdigmi <imamdigmi@gmail.com>
2018-07-09 17:00:49 +0700

    Setup database connection

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # django-elephantsql-demo
 DaaS using Django and ElephantSQL demo
+
+
+## Setup
+Activate virtual envirounment
+```bash
+pipenv shell
+```
+
+Install dependencies
+```bash
+pipenv install
+```
+
+Migrate Schema
+```bash
+python manage.py makemigrations && python manage.py migrate
+```
+
+Create Superuser for accessing Django Admin
+```bash
+python manage.py createsuperuser
+```
+
+# Run!
+```bash
+python manage.py runserver
+```
+
+Navigate to [localhost:8000](http://127.0.0.1:8000/)
--- daas_demo/settings.py
+++ daas_demo/settings.py
@@ -74,9 +74,17 @@ WSGI_APPLICATION = 'daas_demo.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bfloebnu',
+        'USER': 'bfloebnu',
+        'PASSWORD': 'bWsB91e90OD3IqDquvKBKNEzFSp_2hLf',
+        'HOST': 'baasu.db.elephantsql.com',
+        'PORT': '',
     }
 }
 
